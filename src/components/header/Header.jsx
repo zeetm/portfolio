c5855cb609e947d1f362ebdd10704168e8123cf5
Zeinab <zeinabtmohamed@gmail.com>
2022-06-01 15:59:15 -0400

    add name and title to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,13 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>Zeinab Mohamed</h1>
+      <h5 className="text-light">Full Stack Developer</h5>
+      </div>
+    </header>
   )
 }
 
